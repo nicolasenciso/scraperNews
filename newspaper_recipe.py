f981a428c662b04daab8a3a8bc17807baecc4637
Nicolas Enciso <nricardoe@unal.edu.co>
2020-06-29 01:58:07 -0500

    added automation of aditional columns such as host and newspaper_uid to the dataframe

---
+++ newspaper_recipe.py
@@ -0,0 +1,54 @@
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
+from urllib.parse import urlparse
+
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+
+def main(filename):
+    logger.info('starting cleaning process')
+
+    df = _read_data(filename)
+    newspaper_uid = _extract_newspaper_uid(filename)
+    df = _add_newspaper_uid_column(df, newspaper_uid)
+    df = _extract_host(df)
+
+    return df
+
+
+def _extract_host(df):
+    logger.info('extracting host from urls')
+    df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
+
+    return df
+
+
+
+def _read_data(filename):
+    logger.info('Reading file {}'.format(filename))
+
+    return pd.read_csv(filename)
+
+def _extract_newspaper_uid(filename):
+    logger.info('extracting newspaper uid')
+    newspaper_uid = filename.split('_')[0]
+
+    logger.info('newspaper uid detected {}'.format(newspaper_uid))
+    return newspaper_uid
+
+def _add_newspaper_uid_column(df, newspaper_uid):
+    logger.info('filling newspaper_uid columns with {}'.format(newspaper_uid))
+    df['newspaper_uid'] = newspaper_uid
+
+    return df
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename',help='The path to the dirty data', type=str)
+
+    args = parser.parse_args()
+    df = main(args.filename)
+    print(df)
--- pandas_platzi.py
+++ pandas_platzi.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import csv
+from urllib.parse import urlparse
 
-el_universal = pd.read_csv('eluniversal_2020_06_28_articles.csv')
+el_universal = pd.read_csv('eluniversal_2020_06_03_articles.csv')
 
 
+el_universal['newspaper_uid'] = 'eluniversal'
+
+el_universal['host'] = el_universal['url'].apply(lambda url: urlparse(url).netloc)
+
+
+test = el_universal['host'].value_counts()
 print(el_universal)
+print(test)
